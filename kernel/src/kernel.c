4d06a36546a2a5cb4c2f0cc8d8a54725a7cb5834
Snax1 <snax1776@gmail.com>
2022-01-05 02:23:15 -0500

    Text Rendering

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -1,3 +1,20 @@
-int _start() {
-    return 123;
+typedef unsigned long long size_t;
+
+typedef struct {
+	void* BaseAddress;
+	size_t BufferSize;
+	unsigned int Width;
+	unsigned int Height;
+	unsigned int PixelsPerScanline;
+} Framebuffer;
+
+void _start(Framebuffer* framebuffer) {
+    unsigned int y = 50;
+	unsigned int BBP = 4;
+
+	for(unsigned int x = 0; x < framebuffer->Width / 2 * BBP; x+=BBP) {
+		*(unsigned int*)(x + (y * framebuffer->PixelsPerScanline * BBP) + framebuffer->BaseAddress) = 0xff00ffff;
+	}
+
+    return;
 }
